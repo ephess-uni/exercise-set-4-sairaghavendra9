82e9b4042bb1aa286b5b7ae080de9e4b5680d1cb
sairaghavendra9 <165325146+sairaghavendra9@users.noreply.github.com>
2024-04-25 18:24:45 -0500

    Update ex_4_1.py

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -15,7 +15,7 @@ FILENAME = get_data_file_path('messages.log')
 
 def num_shutdowns(logfile):
     shutdown_events = get_shutdown_events(logfile)
-    return len(shutdown_events) // 2
+    return len(shutdown_events)
 
 
 # >>>> The code below will call your function and print the results
